e9d3205da4e1dad5b01471c16f9d5177fa2509f2
Adam Matthiesen <amatthiesen@outlook.com>
2026-09-15 00:58:14 -0700

    Add player login handling to initialize player data entries

--- common/src/main/java/dev/matthiesen/cobblehardcoremon/common/CobbleHardcoreMonCommon.java
+++ common/src/main/java/dev/matthiesen/cobblehardcoremon/common/CobbleHardcoreMonCommon.java
@@ -2,6 +2,7 @@ package dev.matthiesen.cobblehardcoremon.common;
 
 import com.cobblemon.mod.common.api.events.CobblemonEvents;
 import dev.matthiesen.cobblehardcoremon.common.config.CobbleHardcoreMonConfig;
+import dev.matthiesen.cobblehardcoremon.common.data.PlayerData;
 import dev.matthiesen.cobblehardcoremon.common.registry.CommandRegistry;
 import dev.matthiesen.cobblehardcoremon.common.handlers.PlayerBattles;
 import dev.matthiesen.cobblehardcoremon.common.handlers.PlayerPokeParty;
@@ -51,6 +52,7 @@ public final class CobbleHardcoreMonCommon extends AbstractCommonMod {
         PlatformEvents.SERVER_STARTED.subscribe(server -> isServerRunning = true);
         PlatformEvents.SERVER_STOPPING.subscribe(server -> isServerRunning = false);
         PlatformEvents.PLAYER_END_TICK.subscribe(PlayerPokeParty::handlePlayerTick);
+        PlatformEvents.PLAYER_JOIN.subscribe(PlayerData::onPlayerLogin);
 
         CobblemonEvents.BATTLE_STARTED_POST.subscribe(PlayerBattles::battleStartedPost);
         CobblemonEvents.BATTLE_FAINTED.subscribe(PlayerBattles::battlePokemonFainted);
